e70ad9fdfbfbc46e8fe142c1a2d14370d5d18ce1
bob frankel <bob@biosbob.biz>
2024-12-04 14:49:36 -0800

    Utils.updateConfig

--- src/gui/Main.ts
+++ src/gui/Main.ts
@@ -5,6 +5,7 @@ import * as Vsc from 'vscode'
 export async function activate(context: Vsc.ExtensionContext) {
     console.log("em-script active")
     await refreshIcons()
+    Utils.updateConfig()
     Vsc.window.showInformationMessage("EM•Script activated")
 }
 
--- src/gui/Utils.ts
+++ src/gui/Utils.ts
@@ -2,6 +2,25 @@ import Fs from "fs";
 import Path from "path";
 import Vsc from "vscode";
 
+import * as JSON5 from 'json5'
+
+export function updateConfig(): void {
+    const file = Path.join(rootPath(), 'tsconfig.json')
+    const json = JSON5.parse(Fs.readFileSync(file, 'utf-8'))
+    json.compilerOptions.paths = {"@EM-SCRIPT": ["./workspace/em.core/em.lang/em-script"]}
+    let wdir = workPath()
+    Fs.readdirSync(wdir).forEach(f1 => {
+        let ppath = Path.join(wdir, f1);
+        if (isPackage(ppath)) Fs.readdirSync(ppath).forEach(f2 => {
+            let bpath = Path.join(ppath, f2);
+            if (Fs.statSync(bpath).isDirectory()) {
+                json.compilerOptions.paths[`@${f2}/*`] = [`./workspace/${f1}/${f2}/*`]
+            }
+        });
+    });
+    Fs.writeFileSync(file, JSON.stringify(json, null, 4), 'utf-8')
+}
+
 export function isPackage(path: string): boolean {
     if (!Fs.existsSync(path)) return false;
     if (!Fs.statSync(path).isDirectory()) return false;
