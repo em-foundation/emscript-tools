338d603b05faf7a710ecd64e7b306fdf216097ee
bob frankel <bob@biosbob.biz>
2024-12-07 05:45:20 -0800

    MetaProg: capture tsconfig paths

--- src/cli/MetaProg.ts
+++ src/cli/MetaProg.ts
@@ -2,6 +2,8 @@ import * as Fs from 'fs';
 import * as Path from 'path';
 import * as Ts from 'typescript';
 
+const OUTDIR = './workspace/.emscript/genjs'
+
 let curProg: Ts.Program
 
 /*
@@ -49,52 +51,57 @@ function loadTsConfig(configPath: string): Ts.ParsedCommandLine {
 }
 */
 
+export function dump(): void {
+    // console.log(curProg.getRootFileNames()[0])
+}
+
+export function emit(): void {
+    const emitResult = curProg.emit();
+    // exec()
+}
+
+export function exec(): void {
+    const tsFile = curProg.getRootFileNames()[0]
+    const jsPath = Path.resolve(OUTDIR, Path.basename(tsFile).replace(/\.ts$/, '.js'));
+    try {
+        require(jsPath)
+    } catch (error) {
+        console.error(`*** execution error: ${error}`)
+    }
+
+}
+
 export function parse(upath: string): void {
+    const cfgHost: Ts.ParseConfigFileHost = {
+        ...Ts.sys,
+        onUnRecoverableConfigFileDiagnostic: (diagnostic) => {
+            console.error(
+                Ts.formatDiagnosticsWithColorAndContext([diagnostic], {
+                    getCanonicalFileName: (fileName) => fileName,
+                    getCurrentDirectory: Ts.sys.getCurrentDirectory,
+                    getNewLine: () => Ts.sys.newLine,
+                })
+            );
+        },
+    };
+    const cfg = Ts.getParsedCommandLineOfConfigFile('./tsconfig.json', {}, cfgHost)
     const options: Ts.CompilerOptions = {
         module: Ts.ModuleKind.CommonJS,
         target: Ts.ScriptTarget.ESNext,
         strict: true,
         esModuleInterop: true,
-        outDir: 'out', // Output directory
-        paths: {
-            "@EM-SCRIPT": [
-                "./em.core/em.lang/em-script"
-            ],
-            "@bob.test/*": [
-                "./bob.pkg/bob.test/*"
-            ],
-            "@em.hal/*": [
-                "./em.core/em.hal/*"
-            ],
-            "@em.lang/*": [
-                "./em.core/em.lang/*"
-            ],
-            "@em.mcu/*": [
-                "./em.core/em.mcu/*"
-            ],
-            "@em.utils/*": [
-                "./em.core/em.utils/*"
-            ],
-            "@ti.distro.cc23xx/*": [
-                "./ti.cc23xx/ti.distro.cc23xx/*"
-            ],
-            "@ti.mcu.cc23xx/*": [
-                "./ti.cc23xx/ti.mcu.cc23xx/*"
-            ]
-        },
-
-    };
+        sourceMap: true,
+        outDir: OUTDIR,
+        paths: cfg!.options.paths!
+    }
     const customCompilerHost: Ts.CompilerHost = {
         ...Ts.createCompilerHost({}),
         getSourceFile: (fileName, languageVersion, onError) => {
             if (fileName.endsWith(".em.ts") && Path.isAbsolute(fileName)) {
-                console.log("Reading source file:", fileName);
+                // console.log("Reading source file:", fileName);
             }
             return Ts.createCompilerHost({}).getSourceFile(fileName, languageVersion, onError);
         },
     };
     curProg = Ts.createProgram([upath], options, customCompilerHost);
-}
-
-export function dump(): void {
 }
