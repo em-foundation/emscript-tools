018169f3e7f4413c7d16f2e0440e2ad4743be903
bob frankel <bob@biosbob.biz>
2024-12-16 17:37:01 -0800

    em-unit semantic token

--- src/ext/SemTok.ts
+++ src/ext/SemTok.ts
@@ -10,12 +10,34 @@ export class Provider implements Vsc.DocumentSemanticTokensProvider {
             /*setParentNodes*/ true
         );
         const builder = new Vsc.SemanticTokensBuilder(legend())
+        const unitSet = new Set<string>()
+        sf.statements.map((stmt) => {
+            if (Ts.isImportDeclaration(stmt)) {
+                const modSpecNode = stmt.moduleSpecifier
+                if (Ts.isStringLiteral(modSpecNode)) {
+                    let modSpec = modSpecNode.text
+                    const iuMatch = modSpec.match(/^@(.+)\.em$/)
+                    if (iuMatch) {
+                        const inMatch = stmt.importClause!.getText(sf).match(/\W*(\w+)$/)
+                        unitSet.add(inMatch![1])
+                    }
+                }
+            }
+            else if (Ts.isVariableStatement(stmt)) {
+                const dtxt = stmt.declarationList.declarations[0].getText(sf)
+                const m = dtxt.match(/^(\w+)\W+(\w+)\.em\$clone\(.*\)$/)
+                if (!m) return
+                unitSet.add(m[1])
+            }
+
+        })
         const visitNode = (node: Ts.Node): void => {
             if (Ts.isIdentifier(node)) {
                 const name = node.text
                 let tokType = ''
                 if (name === 'em') tokType = 'em-ident'
-                if (name === '$$') tokType = 'em-ident'
+                else if (name === '$$') tokType = 'em-ident'
+                else if (unitSet.has(name)) tokType = 'em-unit'
                 else if (name === 'em$clone') tokType = 'em-special'
                 else if (name.match(/^em\$(meta|targ|template)$/)) tokType = 'em-domain'
                 else if (name.match(/^em\$_[CDIRTU]$/)) tokType = 'em-special'
@@ -37,5 +59,5 @@ export class Provider implements Vsc.DocumentSemanticTokensProvider {
 }
 
 export function legend(): Vsc.SemanticTokensLegend {
-    return new Vsc.SemanticTokensLegend(['em-ident', 'em-domain', 'em-special'], []);
+    return new Vsc.SemanticTokensLegend(['em-ident', 'em-domain', 'em-special', 'em-unit'], []);
 }
