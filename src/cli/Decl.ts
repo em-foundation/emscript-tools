52a44f777d51638428dd7a602747507d712cecca
bob frankel <bob@biosbob.biz>
2024-12-19 15:47:08 -0800

    Decl: WIP

--- src/cli/Decl.ts
+++ src/cli/Decl.ts
@@ -1,6 +1,7 @@
 import * as Ts from 'typescript'
 
 import * as Ast from './Ast'
+import * as Expr from './Expr'
 import * as Out from './Out'
 import * as Type from './Type'
 import * as Unit from './Unit'
@@ -10,7 +11,11 @@ export function generate(decl: Ts.Declaration, ud: Unit.Desc) {
         Out.print("%ttypedef %1 %2;\n", Type.make(decl.type, ud), (decl.name as Ts.Identifier).text)
     }
     else if (Ts.isVariableDeclaration(decl)) {
-        Out.print("%t%1 %2;\n", Type.make(decl.type!, ud), (decl.name as Ts.Identifier).text)
+        const cs = ((decl.parent.flags & Ts.NodeFlags.Const) !== 0) ? 'const ' : ''
+        const dn = (decl.name as Ts.Identifier).text
+        const ts = Type.make(decl.type!, ud)
+        const init = decl.initializer ? `= ${Expr.make(decl.initializer, ud)}` : ''
+        Out.print("%t%1%2 %3%4;\n", cs, ts, dn, init)
     }
     else {
         Ast.fail('Decl', decl)
--- src/cli/Expr.ts
+++ src/cli/Expr.ts
@@ -3,6 +3,6 @@ import * as Ts from 'typescript'
 import * as Out from './Out'
 import * as Unit from './Unit'
 
-export function generate(expr: Ts.Node, ud: Unit.Desc) {
-    Out.addText(expr.getText(ud.sf))
+export function make(expr: Ts.Node, ud: Unit.Desc): string {
+    return expr.getText(ud.sf)
 }
