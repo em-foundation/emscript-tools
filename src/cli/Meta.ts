6000308b74046155734087a08fedafa96341ea19
bob frankel <bob@biosbob.biz>
2025-01-14 06:11:02 -0800

    Trans: refactor

--- src/cli/Meta.ts
+++ src/cli/Meta.ts
@@ -5,6 +5,7 @@ import * as Ts from 'typescript'
 import * as Ast from './Ast'
 import * as Err from './Err'
 import * as Session from './Session'
+import * as Trans from './Trans'
 import * as Unit from './Unit'
 
 let curUpath: string
@@ -183,7 +184,13 @@ function transpile(options: Ts.CompilerOptions) {
         const transOut = Ts.transpileModule(ud.sf.getText(ud.sf), {
             compilerOptions: options,
             fileName: ud.sf.fileName,
-            transformers: { before: [exportTransformer, sizeofTransformer(), structTransformer()] },
+            transformers: {
+                before: [
+                    Trans.exportTransformer,
+                    Trans.sizeofTransformer(),
+                    Trans.structTransformer()
+                ]
+            },
         })
         Fs.mkdirSync(`${buildDir}/${Path.dirname(uid)}`, { recursive: true })
         Fs.writeFileSync(`${buildDir}/${uid}.em.js.map`, transOut.sourceMapText!, 'utf-8')
@@ -200,7 +207,11 @@ function transpile(options: Ts.CompilerOptions) {
     const emOut = Ts.transpileModule(emSrc, {
         compilerOptions: options,
         fileName: emInFile,
-        transformers: { before: [exportTransformer] },
+        transformers: {
+            before: [
+                Trans.exportTransformer
+            ]
+        },
     })
     Fs.mkdirSync(`${buildDir}/${Path.dirname(emFile)}`, { recursive: true })
     Fs.writeFileSync(`${buildDir}/${emFile}.js.map`, emOut.sourceMapText!, 'utf-8')
@@ -222,187 +233,4 @@ function tsortUnits(): Array<string> {
     dfs(`${Session.getDistro().bucket}/BuildC`)
     dfs(Session.mkUid(curUpath))
     return res
-}
-
-const exportTransformer: Ts.TransformerFactory<Ts.SourceFile> = () => {
-    return (root) => {
-        const decls: Ts.PropertyAssignment[] = []
-        const updatedStatements = root.statements.map((stmt) => {
-            if (Ts.isVariableStatement(stmt)) {
-                const isExported = stmt.modifiers?.some(
-                    (mod) => mod.kind === Ts.SyntaxKind.ExportKeyword
-                )
-                if (!isExported) {
-                    stmt.declarationList.declarations.forEach((decl) => {
-                        if (Ts.isIdentifier(decl.name)) {
-                            decls.push(
-                                Ts.factory.createPropertyAssignment(
-                                    decl.name,
-                                    Ts.factory.createIdentifier(decl.name.text)
-                                )
-                            )
-                        }
-                    })
-                }
-            }
-            return stmt
-        })
-        const emDeclsConst = Ts.factory.createVariableStatement(
-            [Ts.factory.createModifier(Ts.SyntaxKind.ExportKeyword)],
-            Ts.factory.createVariableDeclarationList(
-                [
-                    Ts.factory.createVariableDeclaration(
-                        Ts.factory.createIdentifier("em$decls"),
-                        undefined,
-                        undefined,
-                        Ts.factory.createObjectLiteralExpression(decls, true)
-                    ),
-                ],
-                Ts.NodeFlags.Const
-            )
-        )
-        const emDeclsExport = Ts.factory.createExportDeclaration(
-            undefined,
-            false,
-            Ts.factory.createNamedExports([
-                Ts.factory.createExportSpecifier(
-                    false, // isTypeOnly
-                    undefined, // propertyName (no alias)
-                    Ts.factory.createIdentifier("em$decls") // name
-                ),
-            ]),
-            undefined
-        )
-        return Ts.factory.updateSourceFile(root, [
-            ...updatedStatements,
-            emDeclsConst,
-            emDeclsExport,
-        ])
-    }
-}
-
-function sizeofTransformer(): Ts.TransformerFactory<Ts.SourceFile> {
-    return (context) => (sourceFile) => {
-        const primitiveSizes: Record<string, number> = {
-            bool_t: 1,
-            i8: 1,
-            i16: 2,
-            i32: 4,
-            u8: 1,
-            u16: 2,
-            u32: 4,
-        }
-        const aliasSizes: Record<string, number> = {}
-
-        function collectAliasSizes(node: Ts.Node): void {
-            if (Ts.isTypeAliasDeclaration(node) && Ts.isIdentifier(node.name)) {
-                const aliasName = node.name.text
-                if (node.type) {
-                    const size = getSizeOfNode(node.type)
-                    if (!Number.isNaN(size)) {
-                        aliasSizes[aliasName] = size
-                    }
-                }
-            }
-            if (Ts.isClassDeclaration(node)) {
-                const extendsClause = node.heritageClauses?.find(
-                    (clause) => clause.token === Ts.SyntaxKind.ExtendsKeyword
-                )
-                if (extendsClause) {
-                    const extendsType = extendsClause.types[0]
-                    if (Ts.isExpressionWithTypeArguments(extendsType) &&
-                        Ts.isIdentifier(extendsType.expression) &&
-                        extendsType.expression.text === "$struct") {
-                        const className = node.name!.text
-                        let size = 0
-                        for (const member of node.members) {
-                            if (Ts.isPropertyDeclaration(member) && member.type) {
-                                size += getSizeOfNode(member.type)
-                            }
-                        }
-                        aliasSizes[className] = size
-                    }
-                }
-            }
-            Ts.forEachChild(node, collectAliasSizes)
-        }
-
-        function getSizeOfNode(node: Ts.TypeNode): number {
-            if (Ts.isTypeReferenceNode(node)) {
-                if (Ts.isIdentifier(node.typeName)) {
-                    const typeName = node.typeName.text
-                    if (primitiveSizes[typeName] !== undefined) {
-                        return primitiveSizes[typeName]
-                    }
-                    if (aliasSizes[typeName] !== undefined) {
-                        return aliasSizes[typeName]
-                    }
-                }
-            }
-            return Number.NaN
-        }
-
-        function visit(node: Ts.Node): Ts.Node {
-            if (Ts.isExpressionWithTypeArguments(node) && Ts.isIdentifier(node.expression) && node.expression.text === "$sizeof") {
-                const typeArg = node.typeArguments?.[0]
-                if (typeArg && Ts.isTypeNode(typeArg)) {
-                    const size = getSizeOfNode(typeArg)
-                    return Ts.factory.createNumericLiteral(size.toString())
-                }
-            }
-            return Ts.visitEachChild(node, visit, context)
-        }
-
-        collectAliasSizes(sourceFile)
-        return Ts.visitNode(sourceFile, visit) as Ts.SourceFile
-    }
-}
-
-function structTransformer(): Ts.TransformerFactory<Ts.SourceFile> {
-    return (context) => (sourceFile) => {
-        function visit(node: Ts.Node): Ts.Node {
-            if (Ts.isClassDeclaration(node)) {
-                const extendsClause = node.heritageClauses?.find(
-                    (clause) => clause.token === Ts.SyntaxKind.ExtendsKeyword
-                )
-                if (extendsClause) {
-                    const extendsType = extendsClause.types[0]
-                    if (Ts.isExpressionWithTypeArguments(extendsType) &&
-                        Ts.isIdentifier(extendsType.expression) &&
-                        extendsType.expression.text === "$struct") {
-                        const className = node.name!.text
-                        const makeMethod = Ts.factory.createMethodDeclaration(
-                            [Ts.factory.createModifier(Ts.SyntaxKind.StaticKeyword)],
-                            undefined,
-                            Ts.factory.createIdentifier("$make"),
-                            undefined,
-                            undefined,
-                            [],
-                            Ts.factory.createTypeReferenceNode(className, []), // return type
-                            Ts.factory.createBlock([
-                                Ts.factory.createReturnStatement(
-                                    Ts.factory.createNewExpression(
-                                        Ts.factory.createThis(),
-                                        [],
-                                        []
-                                    )
-                                )
-                            ])
-                        )
-                        return Ts.factory.updateClassDeclaration(
-                            node,
-                            node.modifiers,
-                            node.name,
-                            node.typeParameters,
-                            undefined,
-                            [...node.members, makeMethod]
-                        )
-                    }
-                }
-            }
-            return Ts.visitEachChild(node, visit, context)
-        }
-
-        return Ts.visitNode(sourceFile, visit) as Ts.SourceFile
-    }
 }
---
+++ src/cli/Trans.ts
@@ -0,0 +1,185 @@
+import * as Ts from 'typescript'
+
+const primitiveSizes: Record<string, number> = {
+    bool_t: 1,
+    i8: 1,
+    i16: 2,
+    i32: 4,
+    u8: 1,
+    u16: 2,
+    u32: 4,
+}
+const aliasSizes: Record<string, number> = {}
+
+export const exportTransformer: Ts.TransformerFactory<Ts.SourceFile> = () => {
+    return (root) => {
+        const decls: Ts.PropertyAssignment[] = []
+        const updatedStatements = root.statements.map((stmt) => {
+            if (Ts.isVariableStatement(stmt)) {
+                const isExported = stmt.modifiers?.some(
+                    (mod) => mod.kind === Ts.SyntaxKind.ExportKeyword
+                )
+                if (!isExported) {
+                    stmt.declarationList.declarations.forEach((decl) => {
+                        if (Ts.isIdentifier(decl.name)) {
+                            decls.push(
+                                Ts.factory.createPropertyAssignment(
+                                    decl.name,
+                                    Ts.factory.createIdentifier(decl.name.text)
+                                )
+                            )
+                        }
+                    })
+                }
+            }
+            return stmt
+        })
+        const emDeclsConst = Ts.factory.createVariableStatement(
+            [Ts.factory.createModifier(Ts.SyntaxKind.ExportKeyword)],
+            Ts.factory.createVariableDeclarationList(
+                [
+                    Ts.factory.createVariableDeclaration(
+                        Ts.factory.createIdentifier("em$decls"),
+                        undefined,
+                        undefined,
+                        Ts.factory.createObjectLiteralExpression(decls, true)
+                    ),
+                ],
+                Ts.NodeFlags.Const
+            )
+        )
+        const emDeclsExport = Ts.factory.createExportDeclaration(
+            undefined,
+            false,
+            Ts.factory.createNamedExports([
+                Ts.factory.createExportSpecifier(
+                    false, // isTypeOnly
+                    undefined, // propertyName (no alias)
+                    Ts.factory.createIdentifier("em$decls") // name
+                ),
+            ]),
+            undefined
+        )
+        return Ts.factory.updateSourceFile(root, [
+            ...updatedStatements,
+            emDeclsConst,
+            emDeclsExport,
+        ])
+    }
+}
+
+export function sizeofTransformer(): Ts.TransformerFactory<Ts.SourceFile> {
+    return (context) => (sourceFile) => {
+
+        function collectAliasSizes(node: Ts.Node): void {
+            if (Ts.isTypeAliasDeclaration(node) && Ts.isIdentifier(node.name)) {
+                const aliasName = node.name.text
+                if (node.type) {
+                    const size = getSizeOfNode(node.type)
+                    if (!Number.isNaN(size)) {
+                        aliasSizes[aliasName] = size
+                    }
+                }
+            }
+            if (Ts.isClassDeclaration(node)) {
+                const extendsClause = node.heritageClauses?.find(
+                    (clause) => clause.token === Ts.SyntaxKind.ExtendsKeyword
+                )
+                if (extendsClause) {
+                    const extendsType = extendsClause.types[0]
+                    if (Ts.isExpressionWithTypeArguments(extendsType) &&
+                        Ts.isIdentifier(extendsType.expression) &&
+                        extendsType.expression.text === "$struct") {
+                        const className = node.name!.text
+                        let size = 0
+                        for (const member of node.members) {
+                            if (Ts.isPropertyDeclaration(member) && member.type) {
+                                size += getSizeOfNode(member.type)
+                            }
+                        }
+                        aliasSizes[className] = size
+                    }
+                }
+            }
+            Ts.forEachChild(node, collectAliasSizes)
+        }
+
+        function getSizeOfNode(node: Ts.TypeNode): number {
+            if (Ts.isTypeReferenceNode(node)) {
+                if (Ts.isIdentifier(node.typeName)) {
+                    const typeName = node.typeName.text
+                    if (primitiveSizes[typeName] !== undefined) {
+                        return primitiveSizes[typeName]
+                    }
+                    if (aliasSizes[typeName] !== undefined) {
+                        return aliasSizes[typeName]
+                    }
+                }
+            }
+            return Number.NaN
+        }
+
+        function visit(node: Ts.Node): Ts.Node {
+            if (Ts.isExpressionWithTypeArguments(node) && Ts.isIdentifier(node.expression) && node.expression.text === "$sizeof") {
+                const typeArg = node.typeArguments?.[0]
+                if (typeArg && Ts.isTypeNode(typeArg)) {
+                    const size = getSizeOfNode(typeArg)
+                    return Ts.factory.createNumericLiteral(size.toString())
+                }
+            }
+            return Ts.visitEachChild(node, visit, context)
+        }
+
+        collectAliasSizes(sourceFile)
+        return Ts.visitNode(sourceFile, visit) as Ts.SourceFile
+    }
+}
+
+export function structTransformer(): Ts.TransformerFactory<Ts.SourceFile> {
+    return (context) => (sourceFile) => {
+        function visit(node: Ts.Node): Ts.Node {
+            if (Ts.isClassDeclaration(node)) {
+                const extendsClause = node.heritageClauses?.find(
+                    (clause) => clause.token === Ts.SyntaxKind.ExtendsKeyword
+                )
+                if (extendsClause) {
+                    const extendsType = extendsClause.types[0]
+                    if (Ts.isExpressionWithTypeArguments(extendsType) &&
+                        Ts.isIdentifier(extendsType.expression) &&
+                        extendsType.expression.text === "$struct") {
+                        const className = node.name!.text
+                        const makeMethod = Ts.factory.createMethodDeclaration(
+                            [Ts.factory.createModifier(Ts.SyntaxKind.StaticKeyword)],
+                            undefined,
+                            Ts.factory.createIdentifier("$make"),
+                            undefined,
+                            undefined,
+                            [],
+                            Ts.factory.createTypeReferenceNode(className, []), // return type
+                            Ts.factory.createBlock([
+                                Ts.factory.createReturnStatement(
+                                    Ts.factory.createNewExpression(
+                                        Ts.factory.createThis(),
+                                        [],
+                                        []
+                                    )
+                                )
+                            ])
+                        )
+                        return Ts.factory.updateClassDeclaration(
+                            node,
+                            node.modifiers,
+                            node.name,
+                            node.typeParameters,
+                            undefined,
+                            [...node.members, makeMethod]
+                        )
+                    }
+                }
+            }
+            return Ts.visitEachChild(node, visit, context)
+        }
+
+        return Ts.visitNode(sourceFile, visit) as Ts.SourceFile
+    }
+}
