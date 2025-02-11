11cfb94047a8710310c928f3b6dd67e13c76e516
bob frankel <bob@biosbob.biz>
2025-02-10 16:07:22 -0800

    releng/release.sh: tested line-by-line

--- releng/release.sh
+++ releng/release.sh
@@ -7,4 +7,7 @@ git checkout main
 git pull
 git merge --squash staging
 git commit -m "squash merge of staging for v${VERS}"
+git tag v${VERS}
+git push origin v${VERS}
+gh release create v26.0.1 -F CHANGELOG.md
 popd
