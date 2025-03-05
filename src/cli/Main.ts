63f8febfc95b0fff6ccf2079ff03602cdafcacaa
bob frankel <bob@biosbob.biz>
2025-03-04 16:01:08 -0800

    prettier: WIP

--- src/cli/Main.ts
+++ src/cli/Main.ts
@@ -13,68 +13,93 @@ import * as Ast from './Ast'
 import * as Format from './Format'
 import * as Markdown from './Markdown'
 import * as Meta from './Meta'
+import * as Prettier from './Prettier'
 import * as Props from './Props'
 import * as Render from './Render'
 import * as Session from './Session'
 import * as Targ from './Targ'
 import * as Unit from './Unit'
 
-let curTab = ""
+let curTab = ''
 
 const CMD = new Commander.Command('emscript')
 
-CMD
-    .version(Session.version())
-    .option('--root <dir>', 'project root directory', '..')
-CMD
-    .command('build')
+CMD.version(Session.version()).option(
+    '--root <dir>',
+    'project root directory',
+    '..'
+)
+CMD.command('build')
     .description('build a unit')
     .option('-a --ast', 'display AST', false)
     .option('-l --load', 'load after build', false)
     .option('-m --meta', 'meta-program only', false)
-    .option('-S --setup-properties <setup-name>', `add definitions '<setup-name>-setup.properties'`)
-    .requiredOption('-u --unit <qualified-name>', '<package-name>/<bundle-name>/<unit-name>')
+    .option(
+        '-S --setup-properties <setup-name>',
+        `add definitions '<setup-name>-setup.properties'`
+    )
+    .requiredOption(
+        '-u --unit <qualified-name>',
+        '<package-name>/<bundle-name>/<unit-name>'
+    )
     .action((opts: any) => doBuild(opts))
-CMD
-    .command('clean')
+CMD.command('clean')
     .description('clean a bundle')
     .action((opts: any) => doClean(opts))
-CMD
-    .command('config')
+CMD.command('config')
     .description('auto configure this project')
-    .option('-S --setup-properties <setup-name>', `add definitions '<setup-name>-setup.properties'`)
+    .option(
+        '-S --setup-properties <setup-name>',
+        `add definitions '<setup-name>-setup.properties'`
+    )
     .action((opts: any) => doConfig(opts))
-CMD
-    .command('fmt')
+CMD.command('fmt')
     .description('format a unit')
-    .requiredOption('-u --unit <qualified-name>', '<package-name>/<bundle-name>/<unit-name>')
+    .requiredOption(
+        '-u --unit <qualified-name>',
+        '<package-name>/<bundle-name>/<unit-name>'
+    )
     .action((opts: any) => doFormat(opts))
-CMD
-    .command('load')
+CMD.command('load')
     .description('load program')
     .action((opts: any) => doLoad(opts))
 
-CMD
-    .command('markdown')
+CMD.command('markdown')
     .description('generate markdown for a package')
     .requiredOption('-o, --outdir <dir>', 'output directory', '.')
     .requiredOption('-p, --package <dir>', 'package directory', '.')
-    .option('-S --setup-properties <setup-name>', `add definitions '<setup-name>-setup.properties'`)
+    .option(
+        '-S --setup-properties <setup-name>',
+        `add definitions '<setup-name>-setup.properties'`
+    )
     .action((opts: any) => doMarkdown(opts))
-CMD
-    .command('parse')
+CMD.command('parse')
     .description('display AST for a unit')
-    .requiredOption('-u --unit <qualified-name>', '<package-name>/<bundle-name>/<unit-name>')
+    .requiredOption(
+        '-u --unit <qualified-name>',
+        '<package-name>/<bundle-name>/<unit-name>'
+    )
     .action((opts: any) => doParse(opts))
-CMD
-    .command('properties')
+CMD.command('prettier')
+    .description("format using 'prettier'")
+    .requiredOption(
+        '-u --unit <qualified-name>',
+        '<package-name>/<bundle-name>/<unit-name>'
+    )
+    .action((opts: any) => doPrettier(opts))
+CMD.command('properties')
     .description('display workspace properties')
-    .option('-S --setup-properties <setup-name>', `add definitions '<setup-name>-setup.properties'`)
+    .option(
+        '-S --setup-properties <setup-name>',
+        `add definitions '<setup-name>-setup.properties'`
+    )
     .action((opts: any) => doProperties(opts))
-CMD
-    .command('render')
+CMD.command('render')
     .description('render a unit')
-    .requiredOption('-u --unit <qualified-name>', '<package-name>/<bundle-name>/<unit-name>')
+    .requiredOption(
+        '-u --unit <qualified-name>',
+        '<package-name>/<bundle-name>/<unit-name>'
+    )
     .option('--verbose', 'additional output', false)
     .action((opts: any) => doRender(opts))
 
@@ -92,7 +117,11 @@ function doBuild(opts: any): void {
         doConfig(opts)
         process.chdir(cwd)
     }
-    Session.activate(getRootDir(), Session.Mode.BUILD, opts.setupProperties ?? '')
+    Session.activate(
+        getRootDir(),
+        Session.Mode.BUILD,
+        opts.setupProperties ?? ''
+    )
     Props.bindProg(Session.mkUid(upath))
     if (!Props.getSetup()) {
         console.error('*** no setup defined')
@@ -105,7 +134,9 @@ function doBuild(opts: any): void {
     const usedCnt = Session.getUnits().size
     const t1 = mkDelta()
     Targ.generate()
-    console.log(`    executed 'em$meta' program, generated 'main.cpp' using [${usedCnt}/${unitCnt}] units in ${t1} seconds`)
+    console.log(
+        `    executed 'em$meta' program, generated 'main.cpp' using [${usedCnt}/${unitCnt}] units in ${t1} seconds`
+    )
     if (opts.meta) return
     console.log(`compiling 'main.cpp' ...`)
     const stdout = Targ.build()
@@ -127,25 +158,35 @@ function doClean(opts: any): void {
 function doConfig(opts: any) {
     const file = Path.join(getRootDir(), 'tsconfig.json')
     const json: any = {
-        extends: "./tsconfig.base.json",
-        compilerOptions: {
-        }
+        extends: './tsconfig.base.json',
+        compilerOptions: {},
     }
-    json.compilerOptions.paths = { "@$$emscript": ["./workspace/em.core/em.lang/emscript"] }
-    Session.activate(getRootDir(), Session.Mode.PROPS, (opts.setupProperties ?? ''))
+    json.compilerOptions.paths = {
+        '@$$emscript': ['./workspace/em.core/em.lang/emscript'],
+    }
+    Session.activate(
+        getRootDir(),
+        Session.Mode.PROPS,
+        opts.setupProperties ?? ''
+    )
     const wdir = Session.getWorkDir()
-    Fs.readdirSync(wdir).forEach(f1 => {
-        let ppath = Path.join(wdir, f1);
-        if (Session.isPackage(ppath)) Fs.readdirSync(ppath).forEach(f2 => {
-            let bpath = Path.join(ppath, f2);
-            if (Fs.statSync(bpath).isDirectory()) {
-                json.compilerOptions.paths[`@${f2}/*`] = [`./workspace/${f1}/${f2}/*`]
-            }
-        });
-    });
+    Fs.readdirSync(wdir).forEach((f1) => {
+        let ppath = Path.join(wdir, f1)
+        if (Session.isPackage(ppath))
+            Fs.readdirSync(ppath).forEach((f2) => {
+                let bpath = Path.join(ppath, f2)
+                if (Fs.statSync(bpath).isDirectory()) {
+                    json.compilerOptions.paths[`@${f2}/*`] = [
+                        `./workspace/${f1}/${f2}/*`,
+                    ]
+                }
+            })
+    })
     if (Session.hasDistro()) {
         const d = Session.getDistro()
-        json.compilerOptions.paths[`@$distro/*`] = [`./workspace/${d.package}/${d.bucket}/*`]
+        json.compilerOptions.paths[`@$distro/*`] = [
+            `./workspace/${d.package}/${d.bucket}/*`,
+        ]
     }
     Fs.writeFileSync(file, JSON.stringify(json, null, 4), 'utf-8')
 }
@@ -163,8 +204,14 @@ function doLoad(opts: any) {
     }
     const prog = Fs.readFileSync(file, 'utf-8')
     Props.bindProg(prog)
-    const board = Fs.readFileSync(Path.join(Session.getBuildDir(), '.BOARD'), 'utf-8')
-    const setup = Fs.readFileSync(Path.join(Session.getBuildDir(), '.SETUP'), 'utf-8')
+    const board = Fs.readFileSync(
+        Path.join(Session.getBuildDir(), '.BOARD'),
+        'utf-8'
+    )
+    const setup = Fs.readFileSync(
+        Path.join(Session.getBuildDir(), '.SETUP'),
+        'utf-8'
+    )
     printProgress('loading', { setup: setup, board: board })
     loadProg()
 }
@@ -176,7 +223,11 @@ function doMarkdown(opts: any) {
 
 function doParse(opts: any): void {
     const ud = mkUnit(opts, 'parsing')
-    ud.sf.statements.forEach(stmt => Ast.printTree(stmt, '    '))
+    ud.sf.statements.forEach((stmt) => Ast.printTree(stmt, '    '))
+}
+
+function doPrettier(opts: any) {
+    Prettier.exec(opts.unit)
 }
 
 function doProperties(opts: any) {
@@ -191,7 +242,10 @@ function doRender(opts: any) {
 }
 
 function loadProg() {
-    let proc = ChildProc.spawnSync('./load.sh', [], { cwd: Session.getBuildDir(), shell: Session.getShellPath() })
+    let proc = ChildProc.spawnSync('./load.sh', [], {
+        cwd: Session.getBuildDir(),
+        shell: Session.getShellPath(),
+    })
     if (proc.status != 0) {
         console.error('*** loader failed')
         process.exit(1)
@@ -217,7 +271,10 @@ function mkUnit(opts: any, action: string): Unit.Desc {
     return ud
 }
 
-function printProgress(label: string, using: { setup: string, board: string } | boolean = false) {
+function printProgress(
+    label: string,
+    using: { setup: string; board: string } | boolean = false
+) {
     console.log(`${label} '${Props.getProg()}' ...`)
     if (using === false) return
     const board = using === true ? Props.getBoardKind() : using.board
@@ -226,20 +283,26 @@ function printProgress(label: string, using: { setup: string, board: string } | 
 }
 
 function printSha32() {
-    const txt = Fs.readFileSync(Path.join(Session.getBuildDir(), '.out', 'main.out.hex'), 'utf-8')
-    const hash = Crypto.createHash('sha256').update(txt).digest('hex').slice(0, 8)
+    const txt = Fs.readFileSync(
+        Path.join(Session.getBuildDir(), '.out', 'main.out.hex'),
+        'utf-8'
+    )
+    const hash = Crypto.createHash('sha256')
+        .update(txt)
+        .digest('hex')
+        .slice(0, 8)
     console.log(`    image sha32: ${hash}`)
 }
 
 function printSize(stdout: string) {
-    const lines = stdout.split('\n').filter(ln => ln.match(/^\s*\d/))
+    const lines = stdout.split('\n').filter((ln) => ln.match(/^\s*\d/))
     const map = new Map<string, number>([
         ['.text', 0],
         ['.const', 0],
         ['.data', 0],
         ['.bss', 0],
     ])
-    lines.forEach(ln => {
+    lines.forEach((ln) => {
         const words = ln.split(/\s+/)
         const sect = words[2]
         if (map.has(sect)) map.set(sect, Number('0x' + words[3]))
@@ -248,5 +311,7 @@ function printSize(stdout: string) {
     const constSz = map.get('.const')
     const dataSz = map.get('.data')
     const bssSz = map.get('.bss')
-    console.log(`    image size: text (${textSz}) + const (${constSz}) + data (${dataSz}) + bss (${bssSz})`)
+    console.log(
+        `    image size: text (${textSz}) + const (${constSz}) + data (${dataSz}) + bss (${bssSz})`
+    )
 }
