3a883b7da41066377e364948f9f18f22c603b989
bob frankel <bob@biosbob.biz>
2025-03-16 20:45:21 -0700

    Utils: preserve Board if possible

--- src/ext/Utils.ts
+++ src/ext/Utils.ts
@@ -112,7 +112,18 @@ export const setupC = new class Setup extends StatusItem {
         return mkSetupNames().map(sn => `${Setup.PRE}${sn}`)
     }
     async setAux(name: string) {
-        const brd = !name ? '' : curPropMap.get(Props.PROP_BOARD) ?? ''
+        let brd = ''
+        const cur_brd = boardC.get()
+        if (name) {
+            brd = curPropMap.get(Props.PROP_BOARD) ?? ''
+            for (const b of boardC.pickList()) {
+                const bn = b.split('  ')[1]
+                if (cur_brd == bn) {
+                    brd = cur_brd
+                    break
+                }
+            }
+        }
         await boardC.set(brd)
     }
 }
