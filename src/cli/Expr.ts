e9d99abad8189313d4ed8e4d054354d7e80d5303
bob frankel <bob@biosbob.biz>
2025-06-09 11:05:24 -0700

    $proxy2 chains

--- src/cli/Expr.ts
+++ src/cli/Expr.ts
@@ -50,22 +50,27 @@ export function make(expr: Ts.Expression): string {
         // const DEBUG = sa[0] == 'this'
         // const DEBUG = txt.startsWith('AppLed.$$.on')
         // const DEBUG = txt.startsWith('e.$$.tempCoeff')
-        const DEBUG = txt.startsWith('Prx') || txt.startsWith('Prx2.')
+        const DEBUG = txt.startsWith('Common.BusyWait.wait')
         // const DEBUG = false
-        if (DEBUG) console.log('*** 0')
+        if (DEBUG) console.log(`*** 0    kind = ${kind}, len = ${sa.length}`)
         if (DEBUG) console.log(Targ.context().ud.id)
         if (DEBUG) console.log(Targ.context().ud.imports)
         if (DEBUG) console.log(txt, Ast.getTypeExpr(tc, expr.name))
         if (DEBUG) console.log(txt, texp)
-        if (DEBUG) console.log(kind)
 
         if (sa[0] == '$R') {
             return mkReg(sa)
         }
+        else if (texp.startsWith('em$proxy2_t<')) {
+            if (DEBUG) console.log('*** 3')
+            return `${make(expr.expression)}::${expr.name.text}`
+        }
         else if (sa.length == 2 && texp.startsWith('em$proxy2_t<')) {
+            if (DEBUG) console.log('*** 3')
             return `${sa[0]}::${sa[1]}`
         }
-        else if (kind != 'NONE') {
+        else if (kind != 'NONE' && kind != 'PROXY') {
+            if (DEBUG) console.log('*** 4')
             return sa.join('.')
         }
         else if (tc.getTypeAtLocation(expr.expression).isClass()) {
@@ -80,12 +85,11 @@ export function make(expr: Ts.Expression): string {
             return `${make(expr.expression)}.${expr.name.text}`
         }
         else {
-            if (DEBUG) console.log(`*** 2    tn = ${texp}, sa.len = ${sa.length}`)
+            const op = mkSelOp(texp)
+            if (DEBUG) console.log(`*** 2    op = ${op}`)
             // const sym = tc.getTypeAtLocation(expr.expression).getSymbol()
             // console.log(tn, sym?.flags)
             if (sa.length == 2 && texp == 'any' && sa[1] == '$$') return sa[0]  // em$BoxedVal
-            const op = mkSelOp(texp)
-            // console.log(`op = '${op}, tn = ${tn}`)
             if (op == '::') {
                 return sa.join(op)
             }
