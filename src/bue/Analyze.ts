7b82dd68ccf56007b6813d07de3b88d649760df7
bob frankel <bob@biosbob.biz>
2025-07-13 15:55:26 -0700

    Analyze.Options

--- src/bue/Analyze.ts
+++ src/bue/Analyze.ts
@@ -15,9 +15,18 @@ interface Marker {
     end: number
 }
 
-class Signal {
+class Options {
+    readonly sample_rate: number = 1_000_000 // 1 MHz
+    readonly event_thresh: number = 0.0001
+    event_min_dt: number = 0.001 // 1 ms
+    constructor(init?: Partial<Options>) {
+        Object.assign(this, init)
+    }
+}
+
+export class Signal {
     private data: Float32Array<ArrayBuffer>
-    constructor(readonly kind: SigKind, readonly rate: number = 1_000_000, dir: string = '.') {
+    constructor(readonly kind: SigKind, dir: string = '.', readonly opts: Options = new Options) {
         const buf = Fs.readFileSync(Path.join(dir, `${kind}.bin`))
         const cnt = buf.length / 4
         this.data = new Float32Array(cnt)
@@ -25,19 +34,22 @@ class Signal {
             this.data[i] = buf.readFloatLE(i * 4)
         }
     }
-    get duration(): number { return this.length / this.rate }
+    get duration(): number { return this.length / this.opts.sample_rate }
     get length(): number { return this.data.length }
+    get sample_rate(): number { return this.opts.sample_rate }
     get units(): string { return sig_units.get(this.kind)! }
     average(): number {
         const sum = this.data.reduce((a, b) => a + b, 0)
         const avg = sum / this.length
         return avg
     }
-    findEvents(thresh: number, dt: number): Marker[] {
+    findEvents(): Marker[] {
+        const thresh = this.opts.event_thresh
+        const dt = this.opts.event_min_dt
+        const min_width = Math.round(dt * this.opts.sample_rate)
         let res = new Array<Marker>()
         let in_event = false
         let start = 0
-        const min_width = Math.round(dt * this.rate)
         for (let i = 0; i < this.length; i++) {
             const val = this.data[i]
             if (!in_event && val >= thresh) {
@@ -59,7 +71,7 @@ export function exec(opts: any) {
     console.log(`length = ${I_sig.length}`)
     console.log(`duration = ${I_sig.duration} s`)
     console.log(`average = ${toEng(I_sig.average(), I_sig.units)}`)
-    for (const [i, evt] of I_sig.findEvents(0.0001, 0.001).entries()) {
+    for (const [i, evt] of I_sig.findEvents().entries()) {
         console.log(`event ${i}: ${evt.end - evt.start} µs`)
     }
 }
