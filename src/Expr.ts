767aa5c8004caa51e949bb3f78912d5ef83b6960
bob frankel <bob@biosbob.biz>
2026-04-27 09:31:46 -0700

    static_cast for $cast2

--- src/Expr.ts
+++ src/Expr.ts
@@ -219,7 +219,7 @@ function mkCast2(expr: Ts.CallExpression, txt: string): string | null {
     if (!txt.startsWith('$cast2')) return null
     const ts = Type.make(expr.typeArguments![0])
     const arg = make(expr.arguments[0])
-    return `reinterpret_cast<${ts}>(${arg})`
+    return `static_cast<${ts}>(${arg})`
 }
 
 function mkDbg(expr: Ts.Expression, txt: string): string | null {
